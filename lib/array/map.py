0499c64802035d7c4c495e03fe35c4be58c22594
josex2r <jlrepresa@gmail.com>
2022-01-31 20:35:24 +0100

    docs: map

--- lib/array/map.py
+++ lib/array/map.py
@@ -18,8 +18,19 @@ class Callback(Protocol[T_contra, K_co]):
     ) -> K_co: ...
 
 
-def map(array: Iterable[T], func: Callback[T, K]) -> List[K]:
+def map(iterable: Iterable[T], func: Callback[T, K]) -> List[K]:
     """
     Returns a new array with the results of calling func on each element.
+
+    Args:
+        iterable: The iterable to map.
+        func: The callback which transforms each value.
+
+        Returns:
+            A List with each value result.
+
+        Examples:
+            >>> map([1, 2, 3], lambda x: x * 2)
+            [2, 4, 6]
     """
-    return [func(value, index) for index, value in enumerate(array)]
+    return [func(value, index) for index, value in enumerate(iterable)]
