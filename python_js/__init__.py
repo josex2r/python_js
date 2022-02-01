6463b485c153dcbcc836ad3ac13529826efd3ab7
josex2r <jlrepresa@gmail.com>
2022-02-01 10:08:59 +0100

    ci: semantic-rellease

--- python_js/__init__.py
+++ python_js/__init__.py
@@ -4,5 +4,4 @@ python_js is a simple Python module which ports some JS functions to Python.
 This is not production safe and it's made for learning purposes.
 """
 
-__version__ = '0.1.0'
 __author__ = 'Jose Luis Represa'
